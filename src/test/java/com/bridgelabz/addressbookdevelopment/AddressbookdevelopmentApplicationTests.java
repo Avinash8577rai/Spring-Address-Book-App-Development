6d0a9cfec2eeeda96b0ddb89709a35f36236c1ae
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-03-01 15:55:48 +0530

    [Avinash] add a empty project of Address book app develpment using spring boot

---
+++ src/test/java/com/bridgelabz/addressbookdevelopment/AddressbookdevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbookdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressbookdevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
